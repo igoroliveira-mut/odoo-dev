fae71930f7519d06ea6e053f65c2eca755b149f7
igoroliveira-mut <igor.oliveira@mutualizo.com>
2025-09-01 17:41:35 -0300

    ajustes no ambiente

--- .vscode/postCreateCommand.sh
+++ .vscode/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s /opt/odoo/ /workspace/extra-addons/
+ln -s /opt/odoo/ /workspace
 pip3 install -r /workspace/extra-addons/l10n-brazil/requirements.txt --quiet
 pip3 install -r /workspace/extra-addons/MutualizoAddons/requirements.txt --quiet
 pip3 install -r /workspace/requirements.txt --quiet
